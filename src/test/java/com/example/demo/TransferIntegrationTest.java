28fba3632fd0e5d0e5aec6bab359f103f3f6cd26
Virg29 <256in4@gmail.com>
2026-05-29 04:05:17 +0300

    chore: readme and MediaType import fix

--- src/test/java/com/example/demo/TransferIntegrationTest.java
+++ src/test/java/com/example/demo/TransferIntegrationTest.java
@@ -8,6 +8,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.webmvc.test.autoconfigure.AutoConfigureMockMvc;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
 import org.springframework.test.web.servlet.MockMvc;
